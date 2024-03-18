6e59970c9b51e6cfe49a68369ae8aeea97f50520
Marszyygreat <159394182+Marszyygreat@users.noreply.github.com>
2024-03-18 15:17:29 +0700

    Create __init__.py

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,11 @@
+# Ayra - UserBot
+# Copyright (C) 2021-2022 senpai80
+#
+# This file is a part of < https://github.com/senpai80/Ayra/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+
+from Medoy._misc._decorators import ayra_cmd
+from Medoy.fns.helper import inline_mention
+
+from .. import *
